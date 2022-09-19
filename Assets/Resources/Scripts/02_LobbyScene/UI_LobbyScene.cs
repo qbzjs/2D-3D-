c1e5689e2e1a84f22d69fbb48fb1ab8ad4c4bcf7
ArshesSH <105101004ksh@gmail.com>
2022-09-19 23:05:50 +0900

    TID-9 Update LobbyScene Matching UI

--- Assets/Resources/Scripts/02_LobbyScene/UI_LobbyScene.cs
+++ Assets/Resources/Scripts/02_LobbyScene/UI_LobbyScene.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 using Photon.Pun;
 using Photon.Realtime;
@@ -8,17 +10,36 @@ using Photon.Realtime;
 public class UI_LobbyScene : MonoBehaviourPunCallbacks
 {
     #region Public Fields
+    [Header("Room Option")]
+    [SerializeField]
+    private byte maxPlayerCount = 5;
 
+    [Header("UI Objects")]
     [SerializeField]
     private GameObject lobbyObj;
     [SerializeField]
     private GameObject lobbyGameObj;
+    [SerializeField]
+    private GameObject matchingObj;
 
+    [Header("Loading UI Setting")]
+    [SerializeField]
+    private TextMeshProUGUI userCntTMP; 
+    [SerializeField]
+    private Sprite refSpritePlayerOn;
+    [SerializeField]
+    private Sprite refSpritePlayerOff;
+    [SerializeField]
+    private Image[] images;
+    [SerializeField]
+    private GameObject cancelButtonObj;
 
     #endregion
 
 
     #region Private Fields
+    int curPlayerCnt = 0;
+    bool isJoinedRoom = false;
     #endregion
 
 
@@ -27,10 +48,24 @@ public class UI_LobbyScene : MonoBehaviourPunCallbacks
     {
         lobbyObj.SetActive( true );
         lobbyGameObj.SetActive( false );
+        matchingObj.SetActive(false);
     }
     private void Update()
     {
-	
+        if(isJoinedRoom)
+        {
+            curPlayerCnt = PhotonNetwork.CurrentRoom.PlayerCount;
+            Debug.Log($"PlayerCount: {curPlayerCnt}");
+            ChangePlayerImage();
+            ChangePlayerCountText();
+
+            if (curPlayerCnt == 5)
+            {
+                cancelButtonObj.SetActive(false);
+
+            }
+
+        }
     }
     #endregion
 
@@ -39,12 +74,11 @@ public class UI_LobbyScene : MonoBehaviourPunCallbacks
     {
         base.OnJoinRandomFailed( returnCode, message );
         Debug.Log("OnJoinRandomFailed Called " + message );
-        PhotonNetwork.CreateRoom( CreateRandomRoomName(), new RoomOptions { MaxPlayers = 5 } );
+        PhotonNetwork.CreateRoom( CreateRandomRoomName(), new RoomOptions { MaxPlayers = maxPlayerCount } );
     }
     public override void OnCreatedRoom()
     {
         Debug.Log( $"OnCreatedRoom Called, Room Name: {PhotonNetwork.CurrentRoom.Name}" );
-        GameManager.Instance.InputPlayerFaction( true );
     }
     public override void OnCreateRoomFailed( short returnCode, string message )
     {
@@ -55,7 +89,8 @@ public class UI_LobbyScene : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         Debug.Log( "OnJoindRoom Called" );
-        GameManager.Instance.InputPlayerFaction( false );
+        ActiveMatchingUI();
+        isJoinedRoom = true;
     }
     public override void OnJoinRoomFailed( short returnCode, string message )
     {
@@ -70,27 +105,60 @@ public class UI_LobbyScene : MonoBehaviourPunCallbacks
 
 
     #region Private Methods
-    void OnPlayButton()
+    void ResetUI()
     {
-        lobbyObj.SetActive( false );
-        lobbyGameObj.SetActive( true );
+        lobbyObj.SetActive(false);
+        lobbyGameObj.SetActive(false);
+        matchingObj.SetActive(false);
     }
-
-    void OnReturnButton()
+    void ActiveMatchingUI()
     {
-        lobbyObj.SetActive( true );
-        lobbyGameObj.SetActive( false );
+        ResetUI();
+        matchingObj.SetActive(true);
     }
-
-    void OnQuickMatchButton()
+    void ChangePlayerImage()
     {
-        PhotonNetwork.JoinRandomRoom();
-
+        for(int i = 0; i < curPlayerCnt; ++i)
+        {
+            images[i].sprite = refSpritePlayerOn;
+        }
+        for(int i = curPlayerCnt; i < maxPlayerCount; ++i )
+        {
+            images[i].sprite = refSpritePlayerOff;
+        }
+    }
+    void ChangePlayerCountText()
+    {
+        userCntTMP.text = $"{curPlayerCnt} / {maxPlayerCount}";
     }
     string CreateRandomRoomName(  )
     {
         return $"RandomRoom{PhotonNetwork.CountOfRooms + 1}";
     }
+    #endregion
 
+
+    #region Button Callbacks
+    void OnPlayButton()
+    {
+        ResetUI();
+        lobbyGameObj.SetActive(true);
+    }
+    void OnReturnButton()
+    {
+        ResetUI();
+        lobbyObj.SetActive(true);
+    }
+    void OnMatchingCancelButton()
+    {
+        PhotonNetwork.LeaveRoom();
+        ResetUI();
+        lobbyGameObj.SetActive(true);
+        isJoinedRoom = false;
+    }
+    void OnQuickMatchButton()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
     #endregion
 }
