8184f5b450a2656a7b2a5ed58b216c3207f27b30
ArshesSH <105101004ksh@gmail.com>
2022-09-19 15:38:02 +0900

    TID-9 Add GameStartButton

---
+++ Assets/Resources/Scripts/Launcher.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    #region Public Fields
+    #endregion
+
+
+    #region Private Fields
+    #endregion
+
+
+    #region MonoBehaviour Callbacks
+    private void Start()
+    {
+	
+    }
+    private void Update()
+    {
+	
+    }
+    #endregion
+
+
+    #region Public Methods
+    #endregion
+
+
+    #region Private Methods
+    #endregion
+}
