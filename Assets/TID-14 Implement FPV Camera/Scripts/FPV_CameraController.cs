5537b2ca1f5df857ad6464b3c7438694ea301aea
SUHYEON <37034352+gradeF@users.noreply.github.com>
2022-09-23 17:50:31 +0900

    TID-14 fpv camera update

---
+++ Assets/TID-14 Implement FPV Camera/Scripts/FPV_CameraController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPV_CameraController : MonoBehaviour
+{
+    #region Public Fields
+    #endregion
+
+    #region Private Fields
+    #endregion
+
+    #region MonoBehaviour Callbacks
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+    #endregion
+}
