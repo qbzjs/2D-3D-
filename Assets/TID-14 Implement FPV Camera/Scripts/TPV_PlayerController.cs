dfa135e3b280bcffcbba220de41a563d5551f899
이수현/LEE SU HYEON <37034352+gradeF@users.noreply.github.com>
2022-09-26 23:15:21 +0900

    TID-14 added TPV_Player Scripts

---
+++ Assets/TID-14 Implement FPV Camera/Scripts/TPV_PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPV_PlayerController : MonoBehaviour
+{
+    #region Public Field
+    #endregion
+
+    #region Private Field
+    #endregion
+
+    #region Monobehaviour CallBacks
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+    #endregion
+}
