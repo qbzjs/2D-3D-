2362e8a08863d5e5e61558b439aaaa57e9ba4a1d
SUHYEON <37034352+gradeF@users.noreply.github.com>
2022-09-27 11:54:58 +0900

    TID-17 added input Manager and Player InputManager

---
+++ Assets/TID-17-implement-player-character/Resources/Scripts/PlayerInputManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputManager : MonoBehaviour
+{
+    #region Public Fields
+    public PlayerControlls playerControll;
+    public static PlayerInputManager instance { get { return _instance; } }
+    #endregion
+
+    #region Private Fields
+    private static PlayerInputManager _instance;
+    #endregion
+
+    #region MonoBehaviour Callbacks
+    void Awake()
+    {
+        if (_instance == null && this != _instance)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+        playerControll = new PlayerControlls();
+    }
+    #endregion
+
+    #region Public Methods
+    public Vector2 GetPlayerMove()
+    {
+        return playerControll.Player.Movement.ReadValue<Vector2>();
+    }
+    #endregion
+
+    #region Private Methods
+    private void OnEnable()
+    {
+        playerControll.Enable();
+    }
+    private void OnDisable()
+    {
+        playerControll.Disable();
+    }
+    #endregion
+}
