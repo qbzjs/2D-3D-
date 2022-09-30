69857c82cd9a2a925efc7f398eec3a3eac64339f
ArshesSH <105101004ksh@gmail.com>
2022-09-30 14:24:04 +0900

    TID-21 Add Diagrams

--- Assets/Resources/Prototype/Main/Resources/Scripts/SceneScript/LoadingNetworkScene/LoadSceneNetwork.cs
+++ Assets/Resources/Prototype/Main/Resources/Scripts/SceneScript/LoadingNetworkScene/LoadSceneNetwork.cs
@@ -8,21 +8,11 @@ using Photon.Pun;
 
 public class LoadSceneNetwork : LoadScene
 {
-    #region Public Fields
-    #endregion
-
-
-    #region Private Fields
-    #endregion
-
-
     #region MonoBehaviour Callbacks
     protected override void Start()
     {
         if ( PhotonNetwork.IsMasterClient )
         {
-            //base.Start();
-            //PhotonNetwork.LoadSceneAsync( GameManager.Instance.NextSceneName );
             Invoke( "LoadScene", maxDelayTime );
         }
     }
@@ -42,33 +32,5 @@ public class LoadSceneNetwork : LoadScene
     {
         PhotonNetwork.LoadSceneAsync( GameManager.Instance.NextSceneName );
     }
-    #endregion
-
-    #region IEnumerators
-    //public override IEnumerator LoadingNextScene( string sceneName )
-    //{
-    //    async = PhotonNetwork.LoadSceneAsync( sceneName );
-    //    PhotonNetwork.SetSceneActivation( false );
-
-    //    while ( async.progress < 0.9f )
-    //    {
-    //        slider.value = async.progress;
-    //        yield return true;
-    //    }
-
-    //    while ( async.progress >= 0.9f )
-    //    {
-    //        slider.value = async.progress;
-    //        yield return new WaitForSeconds( 0.1f );
-    //        if ( delayTimer >= maxDelayTime )
-    //        {
-    //            break;
-    //        }
-    //    }
-    //    PhotonNetwork.SetSceneActivation( true );
-    //    yield return true;
-    //}
-
-  
     #endregion
 }
