0201dae77adf09ed80cc8de80d80b4424142259c
SUHYEON <37034352+gradeF@users.noreply.github.com>
2022-09-30 17:04:19 +0900

    TID-37 Fixed All Bug

--- Assets/Resources/Prototype/TID-17-implement-player-character/Resources/Scripts/C#/TID-17/TPV_PlayerController.cs
+++ Assets/Resources/Prototype/TID-17-implement-player-character/Resources/Scripts/C#/TID-17/TPV_PlayerController.cs
@@ -32,9 +32,9 @@ public class TPV_PlayerController : MonoBehaviour
     }
     void FixedUpdate()
     {
-        Movement();
-        Rotation();
-        //ControllMove();
+        //Movement();
+        //Rotation();
+        ControllMove();
     }
     #endregion
 
@@ -42,26 +42,11 @@ public class TPV_PlayerController : MonoBehaviour
     #endregion
 
     #region Private Methods
-    private void Movement()
-    {
-        Vector2 currentInput = TPV_PlayerInputManager.instance.GetTPVPlayerMove() * walkSpeed;
-        Debug.Log(currentInput);
-        Vector3 dir = new Vector3(currentInput.x, 0f, currentInput.y);
-        dir = Camera.main.transform.forward * dir.z + Camera.main.transform.right * dir.x;
-
-        rd.velocity = dir;
-    }
-    private void Rotation()
-    {
-        turn.x += Input.GetAxis("Mouse X") * rotSpeed;
-        turn.y += Input.GetAxis("Mouse Y") * rotSpeed;
-        this.gameObject.transform.localRotation = Quaternion.Euler(0, turn.x, 0);
-    }
     private void ControllMove()
     {
-        Vector2 currentInput = PlayerInputManager.instance.GetPlayerMove() * playerSpeed;
-        Debug.Log(currentInput);
-        Vector3 dir = new Vector3(currentInput.x, 0f, currentInput.y);
+        Vector2 currentInput = TPV_PlayerInputManager.instance.GetTPVPlayerMove() * playerSpeed * Time.deltaTime;
+        
+        Vector3 dir = new Vector3(-currentInput.x, currentInput.x, 0.0f );
         dir = Camera.main.transform.forward * dir.z + Camera.main.transform.right * dir.x;
         if (dir != Vector3.zero)
         {
