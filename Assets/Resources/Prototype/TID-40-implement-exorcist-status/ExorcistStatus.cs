3ce6e3337ef1386f361f9a36d6e42e9e1a855335
SUHYEON <37034352+gradeF@users.noreply.github.com>
2022-10-02 15:44:00 +0900

    TID-40 update exorcist status scripts

---
+++ Assets/Resources/Prototype/TID-40-implement-exorcist-status/ExorcistStatus.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExorcistStatus : MonoBehaviour
+{
+    #region Public Fields
+    public float moveSpeed = 1.0f;
+    public float attackSpeed = 1.0f;
+    public int offensePower = 1;
+    public float interactionSpeed = 1.0f;
+    public float projectileSpeed = 1.0f;
+    #endregion
+
+    #region Private Fields
+    #endregion
+
+    #region MonoBehaviour Callbacks
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+    #endregion
+}
+ 
