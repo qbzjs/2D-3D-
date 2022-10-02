f85667d129628ad26915741784aa848011d856ba
SUHYEON <37034352+gradeF@users.noreply.github.com>
2022-10-02 16:05:03 +0900

    TID-40 Fixed ExorcistStatus .cs file

--- Assets/Resources/Prototype/TID-40-implement-exorcist-status/ExorcistStatus.cs
+++ Assets/Resources/Prototype/TID-40-implement-exorcist-status/ExorcistStatus.cs
@@ -2,35 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExorcistStatus : MonoBehaviour
+[System.Serializable]
+public struct ExorcistStatus
 {
-    #region Public Fields
-    public float moveSpeed = 1.0f;
-    public float attackSpeed = 1.0f;
-    public int offensePower = 1;
-    public float interactionSpeed = 1.0f;
-    public float projectileSpeed = 1.0f;
-    #endregion
-
-    #region Private Fields
-    #endregion
-
-    #region MonoBehaviour Callbacks
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-    #endregion
-
-    #region Public Methods
-    #endregion
-
-    #region Private Methods
-    #endregion
+    float moveSpeed;
+    float attackSpeed;
+    int offensePower;
+    float interactionSpeed;
+    float projectileSpeed;
 }
- 
