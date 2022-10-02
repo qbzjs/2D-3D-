f96231470c8444e957072e2725296c24d84248d3
ArshesSH <105101004ksh@gmail.com>
2022-10-02 17:45:12 +0900

    TID-46 Set Default Setting

---
+++ Assets/Resources/Prototype/TID-46-implement-base-camera-controller/Resources/Scirpts/CameraController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KSH_Lib
+{
+    public class CameraController : MonoBehaviour
+    {
+        #region Public Fields
+        #endregion
+
+
+        #region Private Fields
+        #endregion
+
+
+        #region MonoBehaviour Callbacks
+        private void Start()
+        {
+
+        }
+        private void Update()
+        {
+
+        }
+        #endregion
+
+
+        #region Public Methods
+        #endregion
+
+
+        #region Private Methods
+        #endregion
+    }
+}
+
