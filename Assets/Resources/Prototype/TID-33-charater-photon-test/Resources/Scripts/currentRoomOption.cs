e5a4365f0183c1f6ed34f4264d196f2f8f8ad42e
galicskin <104012720+galicskin@users.noreply.github.com>
2022-10-05 16:20:34 +0900

    TID-50 Enable RoomNumber

---
+++ Assets/Resources/Prototype/TID-33-charater-photon-test/Resources/Scripts/currentRoomOption.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+public class currentRoomOption : MonoBehaviour
+{
+    #region Public Fields
+    #endregion	
+
+    #region Private Fields
+    #endregion	
+
+    #region MonoBehaviour CallBacks
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    #endregion	
+
+    #region Public Methods
+    #endregion	
+
+    #region Private Methods
+    #endregion	
+}
