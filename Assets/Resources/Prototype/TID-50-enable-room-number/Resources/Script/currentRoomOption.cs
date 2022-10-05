79ad650c969846fadef2ce806184cb8ef4cdffd4
galicskin <104012720+galicskin@users.noreply.github.com>
2022-10-05 16:32:48 +0900

    TID-50 Implement RoomNumber

--- Assets/Resources/Prototype/TID-50-enable-room-number/Resources/Script/currentRoomOption.cs
+++ Assets/Resources/Prototype/TID-50-enable-room-number/Resources/Script/currentRoomOption.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using Hashtable = ExitGames.Client.Photon.Hashtable;
+using UnityEngine.UI;
 public class currentRoomOption : MonoBehaviour
 {
     #region Public Fields
+    public Text RoomNumber;
     #endregion	
 
     #region Private Fields
@@ -15,7 +17,8 @@ public class currentRoomOption : MonoBehaviour
     #region MonoBehaviour CallBacks
     void Start()
     {
-        
+        int number = (int)PhotonNetwork.CurrentRoom.CustomProperties["roomNumber"];
+        RoomNumber.text = "RoomNumber : "+number.ToString();
     }
 
     void Update()
