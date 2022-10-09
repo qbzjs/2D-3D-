61c7d9aba8a204d342c17c6220a942dc101d98d8
ArshesSH <105101004ksh@gmail.com>
2022-10-09 10:44:05 +0900

    TID-15 Implement PlayerData for test

---
+++ Assets/Resources/Prototype/Main/Resources/Scripts/Player/TestPlayerData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DEM;
+
+namespace KSH_Lib
+{
+	public struct TestPlayerData
+	{
+        /*--- Public Fields ---*/
+        public RoleType Role;
+        public ulong Score;
+        public float moveSpeed;
+        public float rotateSpeed;
+    }
+}
