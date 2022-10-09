6eb8aad55c4e82f90358162b899dd79b5b2a86e8
galicskin <104012720+galicskin@users.noreply.github.com>
2022-10-09 17:33:47 +0900

    TiD-60 Create Folder, script

---
+++ Assets/Resources/Prototype/TID-60-implement-behaviour-state/Resources/Script/State Script/DarknessBehaviour.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarknessBehaviour : MonoBehaviour
+{
+    #region Public Fields
+    #endregion	
+
+    #region Private Fields
+    #endregion	
+
+    #region MonoBehaviour CallBacks
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    #endregion	
+
+    #region Public Methods
+    #endregion	
+
+    #region Private Methods
+    #endregion	
+}
