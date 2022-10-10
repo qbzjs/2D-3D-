f3650f6e39618a9548fa977aaffa51342967d4db
SUHYEON <37034352+gradeF@users.noreply.github.com>
2022-10-10 16:01:32 +0900

    TID-57 implement interaction ui and button active system

---
+++ Assets/Resources/Prototype/TID-57-implement-purification-box/Resource/Scripts/CylinderMove.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct ExorcistStates
+{
+    public bool hasDoll;
+}
+namespace LSH_Lib{
+    
+    public class CylinderMove : MonoBehaviour
+    {
+        public ExorcistStates states;
+        private void Start()
+        {
+            states.hasDoll = false;
+        }
+        private void Update()
+        {
+            Move();
+        }
+        void Move()
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position += transform.forward * 10.0f * Time.deltaTime;
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(Vector3.up, 50.0f * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(Vector3.down, 50.0f * Time.deltaTime);
+            }
+        }
+        public bool GethasDoll()
+        {
+            return states.hasDoll;
+        }
+    }
+}
