bf7f018ced7fa0dae4b245289a2583a109837a88
SUHYEON <37034352+gradeF@users.noreply.github.com>
2022-10-10 17:55:12 +0900

    TID-57 added slidebar setactive

--- Assets/Resources/Prototype/TID-57-implement-purification-box/Resource/Scripts/BoxController.cs
+++ Assets/Resources/Prototype/TID-57-implement-purification-box/Resource/Scripts/BoxController.cs
@@ -1,59 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 namespace LSH_Lib{
     public class BoxController : MonoBehaviour
     {
         public TMP_Text interactText;
-
+        public GameObject slider;
         [SerializeField]
-        bool isActive = false;
+        bool canInteract = true;
         [SerializeField]
-        bool isFull = false;
+        bool hasDoll = false;
+        bool isclick;
 
         private void Start()
         {
             interactText.enabled = false;
+            slider.SetActive(false);
+        }
+        private void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                isclick = false;
+                slider.SetActive(false);
+                interactText.enabled = true;
+            }
         }
         private void OnTriggerStay(Collider other)
         {
-            if (isActive)
+
+            if (other.CompareTag("Exorcist"))
             {
-                if (other.CompareTag("Exorcist"))
-                { 
+                if (canInteract && !hasDoll)
+                {
                     DoInteraction(true, false);
                 }
             }
 
-            if (isFull)
+            if (other.CompareTag("Doll"))
             {
-                if (other.CompareTag("Doll"))
+                if (hasDoll && !canInteract)
                 {
                     DoInteraction(false, true);
                 }
-            }
+            } 
+            
         }
         private void OnTriggerExit(Collider other)
         {
             interactText.enabled = false;
         }
 
-        void DoInteraction(bool isFull, bool isActive)
+        private void OnGUI()
+        {
+            GUI.Box(new Rect(0, 0, 150, 30), isclick.ToString());
+        }
+
+        void DoInteraction(bool hasDoll, bool canInteract)
         {
             interactText.enabled = true;
             if (Input.GetKey(KeyCode.Mouse0))
             {
-                this.isFull = isFull;
+                isclick = true;
+                slider.SetActive(true);
                 interactText.enabled = false;
-                this.isActive = isActive;
-            }
-            if (Input.GetKeyUp(KeyCode.Mouse0))
-            {
-                interactText.enabled = true;
+                this.hasDoll = hasDoll;
+                this.canInteract = canInteract;
             }
         }
-
     }
 }
