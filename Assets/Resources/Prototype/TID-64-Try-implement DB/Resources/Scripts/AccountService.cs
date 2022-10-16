dcbcc57c219b94c8684fab798e7b4e423ac1d176
ArshesSH <105101004ksh@gmail.com>
2022-10-16 13:11:34 +0900

    TID-64 Implement ServerNetwork.cs, AccountService.cs

--- Assets/Resources/Prototype/TID-64-Try-implement DB/Resources/Scripts/AccountService.cs
+++ Assets/Resources/Prototype/TID-64-Try-implement DB/Resources/Scripts/AccountService.cs
@@ -1,34 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Networking;
 
-public class AccountService : MonoBehaviour
+namespace KSH_Lib
 {
-	/*--- Public Fields ---*/
+
+    public class AccountService : ServerNetworkService
+    {
+
+        /*--- Public Fields ---*/
+        public TMP_InputField idField;
+        public TMP_InputField passwordField;
+
+        /*--- Private Fields ---*/
+        string id;
+        string password;
 
 
-	/*--- Protected Fields ---*/
+        /*--- Public Methods ---*/
+        public void Register()
+        {
+            if (!CheckField())
+            {
+                Debug.Log("ID 및 Password 공란");
+                return;
+            }
+            DoPost("register");
+        }
+        public void Login()
+        {
+            if (!CheckField())
+            {
+                Debug.Log("아이디 비번 공란");
+                return;
+            }
+
+            DoPost("login");
+        }
+        public void Logout()
+        {
+            WWWForm form = new WWWForm();
+            form.AddField("order", "logout");
+            StartCoroutine(Post(form));
+        }
 
 
-	/*--- Private Fields ---*/
+        /*--- Protected Methods ---*/
+        protected override WWWForm InitForm(in string order)
+        {
+            var form = base.InitForm(order);
+            form.AddField("id", id);
+            form.AddField("password", password);
+            return form;
+        }
+        protected override void HandleResponse(in string response)
+        {
+            Response data = JsonUtility.FromJson<Response>(response);
+        }
 
 
-	/*--- MonoBehaviour Callbacks ---*/
-	void Start()
-	{
-		
-	}
-	void Update()
-	{
-		
-	}
+        /*--- Private Methods ---*/
+        bool CheckField()
+        {
+            id = idField.text.Trim();
+            password = passwordField.text.Trim();
 
-
-	/*--- Public Methods ---*/
-
-
-	/*--- Protected Methods ---*/
-
-
-	/*--- Private Methods ---*/
+            if (id == "" || password == "")
+            {
+                return false;
+            }
+            return true;
+        }
+    }
 }
--- Assets/Resources/Prototype/TID-64-Try-implement DB/Resources/Scripts/GoogleSheetManager.cs
+++ Assets/Resources/Prototype/TID-64-Try-implement DB/Resources/Scripts/GoogleSheetManager.cs
@@ -5,100 +5,101 @@ using UnityEngine.UI;
 using UnityEngine.Networking;
 using TMPro;
 
-public class GoogleSheetManager : MonoBehaviour
+namespace KSH_Lib
 {
-    [System.Serializable]
-    public struct Response
+    public class GoogleSheetManager : MonoBehaviour
     {
-        public string order;
-        public string result;
-        public string msg;
-    }
-
-    /*--- Public Fields ---*/
-    public TMP_InputField idField;
-    public TMP_InputField passwordField;
-
-
-	/*--- Private Fields ---*/
-	const string URL = "https://script.google.com/macros/s/AKfycbzyCkVWAbzoIjrM5wSKB-yKaAe93qwKIEJJsDOjX04Ghmt5Uac55V_S-u3tibRVSfE/exec";
-    string id;
-    string password;
-
-    /*--- MonoBehaviour Callbacks ---*/
-
-
-    /*--- Public Methods ---*/
-    public void Register()
-    {
-        if(!CheckField())
+        [System.Serializable]
+        public struct Response
         {
-            Debug.Log( "ID 및 Password 공란" );
-            return;
-        }
-        DoAccountPost( "register" );
-    }
-    public void Login()
-    {
-        if(!CheckField())
-        {
-            Debug.Log( "아이디 비번 공란" );
-            return;
+            public string order;
+            public string result;
+            public string msg;
         }
 
-        DoAccountPost( "login" );
-    }
-    public void Logout()
-    {
-        WWWForm form = new WWWForm();
-        form.AddField("order", "logout");
-        StartCoroutine(Post(form));
-    }
+        /*--- Public Fields ---*/
+        public TMP_InputField idField;
+        public TMP_InputField passwordField;
 
-    /*--- Private Methods ---*/
-    bool CheckField()
-    {
-        id = idField.text.Trim();
-        password = passwordField.text.Trim();
 
-        if(id == "" || password == "")
+        /*--- Private Fields ---*/
+        const string URL = "https://script.google.com/macros/s/AKfycbzyCkVWAbzoIjrM5wSKB-yKaAe93qwKIEJJsDOjX04Ghmt5Uac55V_S-u3tibRVSfE/exec";
+        string id;
+        string password;
+
+        /*--- MonoBehaviour Callbacks ---*/
+
+
+        /*--- Public Methods ---*/
+        public void Register()
         {
-            return false;
-        }
-        return true;
-    }
-    void DoAccountPost(in string orderName)
-    {
-        WWWForm form = new WWWForm();
-        form.AddField("order", orderName);
-        form.AddField("id", id);
-        form.AddField("password", password);
-
-        StartCoroutine(Post(form));
-    }
-    void HandleResponse(in string response)
-    {
-        Response data = JsonUtility.FromJson<Response>(response);
-    }
-
-    IEnumerator Post(WWWForm form)
-    {
-        using (UnityWebRequest www = UnityWebRequest.Post(URL, form))
-        {
-            yield return www.SendWebRequest();
-
-            if(www.isDone)
+            if (!CheckField())
             {
-                string response = www.downloadHandler.text;
-                Debug.Log(response);
-                HandleResponse(response);
+                Debug.Log("ID 및 Password 공란");
+                return;
             }
-            else
+            DoAccountPost("register");
+        }
+        public void Login()
+        {
+            if (!CheckField())
             {
-                Debug.Log( "웹의 응답이 없습니다." );
+                Debug.Log("아이디 비번 공란");
+                return;
+            }
+
+            DoAccountPost("login");
+        }
+        public void Logout()
+        {
+            WWWForm form = new WWWForm();
+            form.AddField("order", "logout");
+            StartCoroutine(Post(form));
+        }
+
+        /*--- Private Methods ---*/
+        bool CheckField()
+        {
+            id = idField.text.Trim();
+            password = passwordField.text.Trim();
+
+            if (id == "" || password == "")
+            {
+                return false;
+            }
+            return true;
+        }
+        void DoAccountPost(in string orderName)
+        {
+            WWWForm form = new WWWForm();
+            form.AddField("order", orderName);
+            form.AddField("id", id);
+            form.AddField("password", password);
+
+            StartCoroutine(Post(form));
+        }
+        void HandleResponse(in string response)
+        {
+            Response data = JsonUtility.FromJson<Response>(response);
+        }
+
+        IEnumerator Post(WWWForm form)
+        {
+            using (UnityWebRequest www = UnityWebRequest.Post(URL, form))
+            {
+                yield return www.SendWebRequest();
+
+                if (www.isDone)
+                {
+                    string response = www.downloadHandler.text;
+                    Debug.Log(response);
+                    HandleResponse(response);
+                }
+                else
+                {
+                    Debug.Log("웹의 응답이 없습니다.");
+                }
             }
         }
     }
-
-
 }
