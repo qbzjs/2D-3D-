41743d1c6e0095c61bae78bfaf7d301f7bfa5171
ArshesSH <105101004ksh@gmail.com>
2022-10-17 17:44:02 +0900

    TID-61 data management system diagram

---
+++ Assets/Resources/Prototype/TID-61-implement-data-management/Resources/Scirpts/Data/RoleType.cs
@@ -0,0 +1,28 @@
+
+namespace KSH_Lib
+{
+	public enum RoleType
+	{
+		Null,
+		Doll,
+		Exorcist
+	}
+	public enum ExorcistType
+	{
+		Photographer,
+		Bishop,
+		Dokkaebi,
+		Priest,
+		Hunter,
+		Count
+	}
+	public enum DollType
+	{
+		Monkey,
+		Penguin,
+		Rabbit,
+		Wolf,
+		Tortoise,
+		Count
+	}
+}
