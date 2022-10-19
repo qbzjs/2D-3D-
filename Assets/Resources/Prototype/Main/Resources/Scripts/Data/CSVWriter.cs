e4b237ba060c7e2cc166ba3ed882b567fb794889
ArshesSH <105101004ksh@gmail.com>
2022-10-19 11:27:14 +0900

    TID-61 Add csv management

---
+++ Assets/Resources/Prototype/Main/Resources/Scripts/Data/CSVWriter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public class CSVWriter
+{
+	/*--- Public Fields ---*/
+	List<Dictionary<string, object>>
+
+	/*--- Protected Fields ---*/
+
+
+	/*--- Private Fields ---*/
+
+
+
+	/*--- Public Methods ---*/
+
+
+	/*--- Protected Methods ---*/
+
+
+	/*--- Private Methods ---*/
+}
