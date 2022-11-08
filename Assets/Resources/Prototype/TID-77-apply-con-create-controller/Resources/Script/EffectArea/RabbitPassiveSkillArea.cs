b18b5d26108493ed6ca6dc314d74be1986448c0f
galicskin <104012720+galicskin@users.noreply.github.com>
2022-11-08 16:27:32 +0900

    TID- Update Rabbit

--- Assets/Resources/Prototype/TID-77-apply-con-create-controller/Resources/Script/EffectArea/RabbitPassiveSkillArea.cs
+++ Assets/Resources/Prototype/TID-77-apply-con-create-controller/Resources/Script/EffectArea/RabbitPassiveSkillArea.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using KSH_Lib.Data;
+using KSH_Lib;
 namespace GHJ_Lib
 {
     public class RabbitPassiveSkillArea : EffectArea
     {
         public RabbitController rabbit;
-        bool canMoveInHide = false;
+        bool isReadyToBuff = false;
+        bool isWaitToBuff = false;
+        bool isOnBuff = false;
+        IEnumerator buffCoroutine;
+        private void OnEnable()
+        {
+            buffCoroutine = OnBuff(5.0f);
+        }
         protected override GameObject FindTargets(Collider other)
         {
             if (other.CompareTag(GameManager.ExorcistTag))
@@ -31,14 +39,22 @@ namespace GHJ_Lib
 
         protected override void OnTriggerEnter(Collider other)
         {
-            var target = FindTargets(other);
+            if (!rabbit.photonView.IsMine)
+            {
+                return;
+            }
+
+                var target = FindTargets(other);
             if (target == null)
             {
                 return;
             }
             if (!targets.Contains(target))
             {
-                rabbit.ChangeMoveFunc(true);
+                if(rabbit.CurBehavior is BvHide)
+                {
+                    StartCoroutine("ReadyToMoveInHide");
+                }
                 targets.Add(target);
             }
         }
@@ -46,24 +62,40 @@ namespace GHJ_Lib
         {
             if (targets.Remove(FindTargets(other)))
             {
-                //move Coroutine
+                if (rabbit.photonView.IsMine)
+                {
+                    isWaitToBuff = false;
+                    StartCoroutine(buffCoroutine);
+                }
             }
         }
-        /*
-        IEnumerator MoveInHide(float time)
+        IEnumerator ReadyToMoveInHide()
         {
-            if (!canMoveInHide)
+            float startTime = Time.time;
+            isWaitToBuff = true;
+            while (Time.time - startTime < 1.0f)
             {
-                canMoveInHide = true;
+                yield return new WaitForEndOfFrame(); // 문서에 시간: 1초
+                if (!isWaitToBuff)
+                {
+                    yield break;
+                }
             }
-
-            while (true)
-            {
-                yield return
-            }
-
-      
+            isReadyToBuff = true;
         }
-        */
+
+        IEnumerator OnBuff(float time) //문서에 버프시간이 5초
+        {
+            if (!isReadyToBuff)
+            {
+                yield break;
+            }
+            isReadyToBuff = false;
+            isOnBuff = true;
+            rabbit.ChangeMoveSpeed(1.5f);
+            yield return new WaitForSeconds(time);
+            isOnBuff = false;
+            rabbit.ChangeMoveSpeed(1.0f);
+        }
     }
 }
