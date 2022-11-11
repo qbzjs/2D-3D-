c148eeb71ee95e587eeeb017d3b6fa69fbe33abb
이수현/LEE SU HYEON <37034352+gradeF@users.noreply.github.com>
2022-11-12 00:14:34 +0900

    TID-91 Update Option UI

--- Assets/Resources/Prototype/TID-91-add-ui/Scripts/OptionUIController.cs
+++ Assets/Resources/Prototype/TID-91-add-ui/Scripts/OptionUIController.cs
@@ -8,6 +8,32 @@ namespace LSH_Lib
 	{
 		[Header("Setting UI")]
 		[SerializeField]
-		GameObject OptionPanel;
-	}
+		GameObject optionPanel;
+
+        [SerializeField]
+        GameObject settingWindow;
+        [SerializeField]
+        GameObject controllWindow;
+
+        private void Start()
+        {
+            optionPanel.SetActive(false);
+            settingWindow.SetActive(true);
+            controllWindow.SetActive(false);
+        }
+        void EnableOptionPanel()
+        {
+            optionPanel.SetActive(true);
+        }
+        void EnableSettingWindow()
+        {
+            controllWindow.SetActive(false);
+            settingWindow.SetActive(true);
+        }
+        void EnableControllWindow()
+        {
+            settingWindow.SetActive(false);
+            controllWindow.SetActive(true);
+        }
+    }
 }
