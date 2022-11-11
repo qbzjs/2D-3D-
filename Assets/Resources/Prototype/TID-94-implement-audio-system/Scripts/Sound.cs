d05cd097c5dc8e2a8deb3a2446e0873c00fa8c43
ArshesSH <105101004ksh@gmail.com>
2022-11-11 09:17:28 +0900

    TID-101 Add Volume mixer but need to have ui

--- Assets/Resources/Prototype/TID-94-implement-audio-system/Scripts/Sound.cs
+++ Assets/Resources/Prototype/TID-94-implement-audio-system/Scripts/Sound.cs
@@ -6,12 +6,18 @@ namespace LSH_Lib
 	[System.Serializable]
 	public class Sound
 	{
+		public enum SoundGroup
+        {
+			BGM,
+			SFX,
+        }
+
 		public string name;
+		public SoundGroup group;
 		public AudioClip clip;
-		[Range(0f, 1f)]
-		public float volume;
-		[Range(-3.0f, 3.0f)]
-		public float pitch;
+		[Range(0f, 1f)] public float volume;
+		[Range(-3.0f, 3.0f)] public float pitch;
+
 		[HideInInspector]
 		public AudioSource source;
 		public bool loop;
