b1d4bbf60bc792187899ea27ed7d0a173a9fb351
SUHYEON <37034352+gradeF@users.noreply.github.com>
2022-11-11 17:02:16 +0900

    TID-91 Update UI

---
+++ Assets/Resources/Prototype/TID-91-add-ui/Scripts/OptionUIController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LSH_Lib
+{
+	public class OptionUIController : MonoBehaviour
+	{
+		[Header("Setting UI")]
+		[SerializeField]
+		GameObject OptionPanel;
+	}
+}
