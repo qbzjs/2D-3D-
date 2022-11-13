9be2c6018046c892646d4403909bf888d56b585f
ArshesSH <105101004ksh@gmail.com>
2022-11-13 23:39:00 +0900

    TID-101 Add Gauged Object to interact with watching

--- Assets/Resources/Prototype/Main/Resources/Scripts/System/InteractSystem/GaugedObj.cs
+++ Assets/Resources/Prototype/Main/Resources/Scripts/System/InteractSystem/GaugedObj.cs
@@ -74,6 +74,11 @@ namespace KSH_Lib
             {
                 return false;
             }
+            if( !controller.IsWatching(gameObject))
+            {
+                promptUI.Inactivate();
+                return false;
+            }
             targetController = controller;
             return true;
         }
