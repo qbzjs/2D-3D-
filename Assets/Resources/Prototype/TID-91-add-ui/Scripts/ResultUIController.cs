85833e2a283b66b6d881a5ce36c5a165aca1d5bb
SUHYEON <37034352+gradeF@users.noreply.github.com>
2022-11-14 11:45:48 +0900

    TID-91 update UI

---
+++ Assets/Resources/Prototype/TID-91-add-ui/Scripts/ResultUIController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using KSH_Lib;
+using KSH_Lib.UI;
+using KSH_Lib.Data;
+using GHJ_Lib;
+namespace LSH_Lib
+{
+    public class ResultUIController : MonoBehaviour
+    {
+        [System.Serializable]
+        public struct PlayerInfor
+        {
+            public Image Backgroud;
+            public Image PlayerIcon;
+            public TextMeshProUGUI NickName;
+            public TextMeshProUGUI RoleType;
+            public Image StatusIcon;
+        }
+        [SerializeField]
+        PlayerInfor[] playerInfors;
+        [SerializeField]
+        PlayerInfor mineInfor; 
+        [SerializeField]
+        Sprite[] backgrounds;
+        [SerializeField]
+        Sprite[] statusIcons;
+        [SerializeField]
+        Sprite[] PlayerIcons;
+        
+        private void Update()
+        {
+            if(DataManager.Instance.PreRoleGroup.Equals(RoleData.RoleGroup.Exorcist))
+            {
+                playerInfors[3].Backgroud.sprite = backgrounds[0];
+            }
+            if(DataManager.Instance.PreRoleGroup.Equals(RoleData.RoleGroup.Doll))
+            {
+                playerInfors[3].Backgroud.sprite = backgrounds[1];
+            }
+        }
+    }
+}
