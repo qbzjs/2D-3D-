da1a48d3704292b1162d1e99bb389eb9b59c77b0
ArshesSH <105101004ksh@gmail.com>
2022-11-14 10:50:09 +0900

    TID-101 roll back control

--- Assets/Resources/Prototype/Main/Resources/Scripts/Player/Controller/BasePlayerController.cs
+++ Assets/Resources/Prototype/Main/Resources/Scripts/Player/Controller/BasePlayerController.cs
@@ -46,9 +46,9 @@ namespace KSH_Lib
 		/*--- MonoBehaviour Callbacks ---*/
 		protected virtual void Awake()
         {
-			//Cursor.lockState = CursorLockMode.Locked;
-			//Cursor.visible = false;
-		}
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
 		protected virtual void Start()
 		{
 			controller = characterObj.GetComponent<CharacterController>();
