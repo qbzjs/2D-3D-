eff686eca77199e2428dea3900a654d1a9659af2
이수현/LEE SU HYEON <37034352+gradeF@users.noreply.github.com>
2022-11-30 02:34:49 +0900

    UIBugFix - Fix inGame UI

--- Assets/Resources/Prototype/TID-52-implement-character-ui/Resources/Script/DollUI.cs
+++ Assets/Resources/Prototype/TID-52-implement-character-ui/Resources/Script/DollUI.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using KSH_Lib;
 using KSH_Lib.Data;
-
+using TMPro;
 using Photon.Pun;
 
 namespace GHJ_Lib
@@ -16,20 +16,33 @@ namespace GHJ_Lib
         public GameObject PlayerUiObject;
         public GameObject[] FriendUiObjects;
 
+        [Header("Sprites")]
+        [SerializeField]
+        Sprite[] iconSprites;
 
         [Header("PlayerHP")]
+        [SerializeField]
+        Image PlayerImage;
+        [SerializeField]
+        TextMeshProUGUI playerName;
         public Slider PlayerDollHP;
         public Slider PlayerDevilHP;
 
         [Header("Friend1 HP")]
+        [SerializeField] Image Friend1Sprite;
+        [SerializeField] TextMeshProUGUI Friend1Name;
         public Slider Friend1DollHP;
         public Slider Friend1DevilHP;
 
         [Header("Friend2 HP")]
+        [SerializeField] Image Friend2Sprite;
+        [SerializeField] TextMeshProUGUI Friend2Name;
         public Slider Friend2DollHP;
         public Slider Friend2DevilHP;
 
         [Header("Friend3 HP")]
+        [SerializeField] Image Friend3Sprite;
+        [SerializeField] TextMeshProUGUI Friend3Name;
         public Slider Friend3DollHP;
         public Slider Friend3DevilHP;
 
@@ -42,6 +55,8 @@ namespace GHJ_Lib
 
         private List<float> maxDollHP = new List<float>();
         private List<float> maxDevilHP = new List<float>();
+        private List<TextMeshProUGUI> friendNickName = new List<TextMeshProUGUI>();
+        private List<Image> friendSprite = new List<Image>();
 
         private int myIdx;
 
@@ -66,9 +81,46 @@ namespace GHJ_Lib
             friendDevilHP.Add(Friend2DevilHP);
             friendDevilHP.Add(Friend3DevilHP);
 
-            StartCoroutine(InitUI());
-        }
+            friendNickName.Add(Friend1Name);
+            friendNickName.Add(Friend2Name);
+            friendNickName.Add(Friend3Name);
 
+            friendSprite.Add(Friend1Sprite);
+            friendSprite.Add(Friend2Sprite);
+            friendSprite.Add(Friend3Sprite);
+            for (int i = 1; i < PhotonNetwork.CurrentRoom.PlayerCount - 1; ++i)
+            {
+                if (i == myIdx)
+                {
+                    PlayerImage.sprite = SetSprite(DataManager.Instance.PlayerDatas[i].roleData.Type);
+                    playerName.text = DataManager.Instance.PlayerDatas[i].accountData.Nickname;
+                }
+                else
+                {
+                    friendSprite[i].sprite = SetSprite(DataManager.Instance.PlayerDatas[i].roleData.Type);
+                    friendNickName[i].text = DataManager.Instance.PlayerDatas[i].accountData.Nickname;
+                }
+            }
+            StartCoroutine(InitUI());
+            
+        }
+        Sprite SetSprite(RoleData.RoleType type)
+        {
+            switch (type)
+            {
+                case RoleData.RoleType.Rabbit:
+                {
+                        return iconSprites[0];
+                }
+                case RoleData.RoleType.Wolf:
+                    {
+                        return iconSprites[1];
+                    }
+                default:
+                    return iconSprites[2];
+                      
+            }
+        }
         private void Update()
         {
             if(!isInited || PlayerUiObject == null || FriendUiObjects == null)
@@ -86,6 +138,8 @@ namespace GHJ_Lib
             {
                 if (myIdx == i)
                 {
+                    PlayerImage.sprite = SetSprite(DataManager.Instance.PlayerDatas[i].roleData.Type);
+                    playerName.text = DataManager.Instance.PlayerDatas[i].accountData.Nickname;
                     PlayerDollHP.value = (DataManager.Instance.PlayerDatas[i].roleData as DollData).DollHP / maxDollHP[i - 1];
                     PlayerDevilHP.value = (DataManager.Instance.PlayerDatas[i].roleData as DollData).DevilHP / maxDevilHP[i - 1];
                     PlayerDollHP.value = (DataManager.Instance.PlayerDatas[i].roleData as DollData).DollHP / maxDollHP[i - 1];
@@ -93,6 +147,8 @@ namespace GHJ_Lib
                 }
                 else
                 {
+                    friendSprite[j].sprite = SetSprite(DataManager.Instance.PlayerDatas[i].roleData.Type);
+                    friendNickName[j].text = DataManager.Instance.PlayerDatas[i].accountData.Nickname;
                     friendDollHP[j].value = (DataManager.Instance.PlayerDatas[i].roleData as DollData).DollHP / maxDollHP[i - 1];
                     friendDevilHP[j].value = (DataManager.Instance.PlayerDatas[i].roleData as DollData).DevilHP / maxDevilHP[i - 1];
                     j++;
@@ -129,6 +185,7 @@ namespace GHJ_Lib
             myIdx = DataManager.Instance.PlayerIdx;
             for (int i = 1; i <= curDollCount; ++i)
             {
+                
                 maxDollHP.Add((DataManager.Instance.PlayerDatas[i].roleData as DollData).DollHP);
                 maxDevilHP.Add((DataManager.Instance.PlayerDatas[i].roleData as DollData).DevilHP);
             }
--- Assets/Resources/Prototype/TID-52-implement-character-ui/Resources/Script/ExorcistUI.cs
+++ Assets/Resources/Prototype/TID-52-implement-character-ui/Resources/Script/ExorcistUI.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using KSH_Lib;
 using KSH_Lib.Data;
-
+using TMPro;
 using Photon.Pun;
 
 namespace GHJ_Lib
@@ -14,19 +14,31 @@ namespace GHJ_Lib
         [Header("UI Objects")]
         public GameObject[] DollUiObjects;
 
+        [Header("Sprites")]
+        [SerializeField]
+        Sprite[] iconSprites;
+
         [Header("Doll1 HP")]
+        public Image Doll1Image;
+        public TextMeshProUGUI Doll1Name;
         public Slider Doll1HP;
         public Slider Devil1HP;
 
         [Header("Doll2 HP")]
+        public Image Doll2Image;
+        public TextMeshProUGUI Doll2Name;
         public Slider Doll2HP;
         public Slider Devil2HP;
 
         [Header("Doll3 HP")]
+        public Image Doll3Image;
+        public TextMeshProUGUI Doll3Name;
         public Slider Doll3HP;
         public Slider Devil3HP;
 
         [Header("Doll4 HP")]
+        public Image Doll4Image;
+        public TextMeshProUGUI Doll4Name;
         public Slider Doll4HP;
         public Slider Devil4HP;
 
@@ -36,6 +48,9 @@ namespace GHJ_Lib
         [Header("Active Skill")]
         public InputActionUI CharacterSkill;
 
+
+        List<Image> dollImage = new List<Image>();
+        List<TextMeshProUGUI> dollName = new List<TextMeshProUGUI>();
         List<Slider> dollHP = new List<Slider>();
         List<Slider> devilHP = new List<Slider>();
         List<float> maxDollHP = new List<float>();
@@ -49,6 +64,16 @@ namespace GHJ_Lib
             DisableUI_All();
             EnableUI();
 
+            dollImage.Add(Doll1Image);
+            dollImage.Add(Doll2Image);
+            dollImage.Add(Doll3Image);
+            dollImage.Add(Doll4Image);
+
+            dollName.Add(Doll1Name);
+            dollName.Add(Doll2Name);
+            dollName.Add(Doll3Name);
+            dollName.Add(Doll4Name);
+
             dollHP.Add(Doll1HP);
             dollHP.Add(Doll2HP);
             dollHP.Add(Doll3HP);
@@ -58,10 +83,31 @@ namespace GHJ_Lib
             devilHP.Add(Devil2HP);
             devilHP.Add(Devil3HP);
             devilHP.Add(Devil4HP);
-
+            for(int i = 1; i<PhotonNetwork.CurrentRoom.PlayerCount-1;++i)
+            {
+                dollImage[i].sprite = SetSprite(DataManager.Instance.PlayerDatas[i].roleData.Type);
+                dollName[i].text = DataManager.Instance.PlayerDatas[i].accountData.Nickname;
+            }
             StartCoroutine(InitUI());
+            
         }
+        Sprite SetSprite(RoleData.RoleType type)
+        {
+            switch (type)
+            {
+                case RoleData.RoleType.Rabbit:
+                    {
+                        return iconSprites[0];
+                    }
+                case RoleData.RoleType.Wolf:
+                    {
+                        return iconSprites[1];
+                    }
+                default:
+                    return iconSprites[2];
 
+            }
+        }
         void Update()
         {
             if(!isInited || DollUiObjects == null)
@@ -76,6 +122,8 @@ namespace GHJ_Lib
 
             for (int i = 1; i < PhotonNetwork.CurrentRoom.PlayerCount; ++i)
             {
+                dollImage[i - 1].sprite = SetSprite(DataManager.Instance.PlayerDatas[i].roleData.Type);
+                dollName[i - 1].text = DataManager.Instance.PlayerDatas[i].accountData.Nickname;
                 dollHP[i - 1].value = (DataManager.Instance.PlayerDatas[i].roleData as DollData).DollHP / maxDollHP[i - 1];
                 devilHP[i - 1].value = (DataManager.Instance.PlayerDatas[i].roleData as DollData).DevilHP / maxDevilHP[i - 1];
             }
